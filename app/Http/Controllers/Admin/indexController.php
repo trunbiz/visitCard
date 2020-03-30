aa4f13ccad76ef70bec8f202a642dde93e7c0051
trantrungnkc@gmail.com <trunglata>
2020-03-30 23:35:56 +0700

    louis commit

--- app/Http/Controllers/Admin/indexController.php
+++ app/Http/Controllers/Admin/indexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth; //use thư viện auth
 
 class indexController extends Controller
 {
@@ -19,9 +20,26 @@ class indexController extends Controller
     {
         return view('front.login');
     }
-    public function checkLogin()
+    public function checkLogin(Request $request)
     {
+        $arr = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+        if ($request->remember == trans('remember.Remember Me')) {
+            $remember = true;
+        } else {
+            $remember = false;
+        }
+        //kiểm tra trường remember có được chọn hay không
 
+        if (Auth::attempt($arr)) {
+
+            return view('admin.index');
+        } else {
+
+           return view('front.login');
+        }
     }
     public function showRegister()
     {
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace'=>'Front'],function (){
     Route::get('/','indexController@indexShow');
+
 });
 Route::group(['namespace'=>'Admin'],function(){
    Route::group(['prefix'=>'admin'],function(){
